82d01be2f31a96353b7be1c3ecba52aa6935c527
Alexandra (Sasha) Fedorova <sasha.fedorova@mongodb.com>
2023-11-18 12:24:57 -0800

    Integrated the C wrapper for C++ map.

---
+++ libCacheSim/dataStructure/map/map.cpp
@@ -0,0 +1,51 @@
+#include "map.hpp"
+
+typedef std::map<std::uint64_t, void*> CppMap;
+
+extern "C" {
+
+Map createMap() {
+    return new CppMap();
+}
+
+int
+insertPair(Map map, const uint64_t key, void* value)
+{
+    if (map) {
+        CppMap* cppMap = static_cast<CppMap*>(map);
+        (*cppMap)[key] = value;
+        return (0);
+    }
+    return (-1);
+}
+
+void *
+getValue(Map map, uint64_t key)
+{
+    if (map) {
+        CppMap* cppMap = static_cast<CppMap*>(map);
+        auto it = cppMap->find(key);
+        if (it != cppMap->end()) {
+            return it->second;
+        }
+    }
+    return NULL;
+}
+
+int
+removePair(Map map, uint64_t key)
+{
+    if (map) {
+        CppMap* cppMap = static_cast<CppMap*>(map);
+        return cppMap->erase(key);
+    }
+    return 0;
+}
+
+void deleteMap(Map map)
+{
+    if (map)
+        delete static_cast<CppMap*>(map);
+}
+
+} // extern "C"
---
+++ libCacheSim/dataStructure/map/map.hpp
@@ -0,0 +1,37 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <cstdint>
+#include <map>
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sys/types.h>
+
+
+/* Define a handle for the C++ map to be used in C */
+typedef void* Map;
+
+/* Create a new map and return its handle */
+Map createMap();
+
+/* Insert a key-value pair into the map */
+int insertPair(Map map, uint64_t key, void* value);
+
+/* Get the value associated with a key from the map */
+void * getValue(Map map, uint64_t);
+
+/* Remove the pair from the map */
+int removePair(Map map, uint64_t key);
+
+/* Delete the map and free resources */
+void deleteMap(Map map);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MAP_HPP
