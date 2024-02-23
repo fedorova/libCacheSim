8cc56398c9c867edc2f2207dfc1716e090dee9bc
Alexandra (Sasha) Fedorova <sasha.fedorova@mongodb.com>
2024-02-23 14:37:18 -0800

    Set the queue size pointer to exclude null entries

--- libCacheSim/cache/eviction/WiredTiger.c
+++ libCacheSim/cache/eviction/WiredTiger.c
@@ -440,8 +440,6 @@ __evict_lru_walk(const cache_t *cache)
     ret = __evict_walk(cache, queue);
     (void) ret; /* No need to check for now. */
 
-    for (int i = 0; i < queue->evict_entries; i++)
-        printf("el[%d]: %p\n", i, queue->elements[i]);
     /*
      * Sort the evict queue and set the number of non-empty elements
      */
@@ -449,9 +447,6 @@ __evict_lru_walk(const cache_t *cache)
 
     qsort(queue->elements, queue->evict_entries, sizeof(cache_obj_t*), __evict_qsort_compare);
     entries = queue->evict_entries;
-
-    for (int i = 0; i < queue->evict_entries; i++)
-        printf("el[%d]: %p\n", i, queue->elements[i]);
 
     /*
      * If we have more entries than the maximum tracked between walks, clear them. Do this before
@@ -461,6 +456,10 @@ __evict_lru_walk(const cache_t *cache)
     INFO("__evict_lru_walk: clearing unneeded entries from a queue with %d entries\n", entries);
     while (entries > WT_EVICT_WALK_BASE)
         queue->elements[--entries] = NULL;
+
+    /* Re-adjust the entries to the first non-empty slot */
+    while (queue->elements[entries-1] == NULL)
+        entries--;
     queue->evict_entries = entries;
     INFO("__evict_lru_walk: ended up with %d entries after clearing\n", entries);
 
@@ -1229,9 +1228,6 @@ __evict_qsort_compare(const void *a_arg, const void *b_arg) {
     b = *(cache_obj_t **)b_arg;
     a_score = (a == NULL ? UINT64_MAX : a->wt_page.evict_score);
     b_score = (b == NULL ? UINT64_MAX : b->wt_page.evict_score);
-
-    printf("a = %p, b = %p, a_score = %lu, b_score = %lu\n", a, b, a_score, b_score);
-    printf("returning %d\n", ((a_score < b_score) ? -1 : (a_score == b_score) ? 0 : 1));
 
     return ((a_score < b_score) ? -1 : (a_score == b_score) ? 0 : 1);
 }
